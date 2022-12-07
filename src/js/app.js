c3dc5df1bbcdc144d38f6591024747ea5bcefd96
Annie <overheads165@gmail.com>
2022-12-07 23:51:43 +0100

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,22 @@
 // гоблин рандомно появляется в рандомной клетке
-const cells = document.querySelectorAll('.cell');
+const cells = document.querySelectorAll(".cell");
 
 const randomCell = Math.floor(Math.random() * cells.length);
-cells[randomCell].classList.add('goblin');
-
-//находим в какой клетке гоблин
-//перемещаем гоблин в другую рандомную клетку за исключением предыдущей и делаем это с интервалом 2 секунды
+cells[randomCell].classList.add("goblin");
 
 setInterval(() => {
-  const goblinCell = document.querySelector('.goblin');
-  goblinCell.classList.remove('goblin');
+  const goblinCell = document.querySelector(".goblin");
 
-  const randomCell = Math.floor(Math.random() * cells.length);
-  cells[randomCell].classList.add('goblin');
+  // найти индекс клетки гоблина в массиве cells
+  const goblinCellIndex = Array.from(cells).indexOf(goblinCell);
+  
+  goblinCell.classList.remove("goblin");
+  
+  // рандомно выбрать индекс клетки куда переместить гоблина чтобы он не оказался в той же клетке
+  let randomCell = Math.floor(Math.random() * cells.length);
+  while (randomCell === goblinCellIndex) {
+    randomCell = Math.floor(Math.random() * cells.length);
+  }
+
+  cells[randomCell].classList.add("goblin");
 }, 1000);
