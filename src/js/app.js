1796e234808183f2dadea7ffd20ba592833caf48
Annie <overheads165@gmail.com>
2022-12-07 23:19:42 +0100

    goblin jump

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,11 @@ cells[randomCell].classList.add('goblin');
 
 //находим в какой клетке гоблин
 //перемещаем гоблин в другую рандомную клетку за исключением предыдущей и делаем это с интервалом 2 секунды
+
+setInterval(() => {
+  const goblinCell = document.querySelector('.goblin');
+  goblinCell.classList.remove('goblin');
+
+  const randomCell = Math.floor(Math.random() * cells.length);
+  cells[randomCell].classList.add('goblin');
+}, 1000);
